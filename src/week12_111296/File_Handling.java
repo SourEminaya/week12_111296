8128b9bb0ebe429d13a36030964399b747e04481
NehemiaOroh <orohnehemia@gmail.com>
2025-11-26 10:57:36 +0700

    Read myfile.txt

--- src/week12_111296/File_Handling.java
+++ src/week12_111296/File_Handling.java
@@ -1,6 +1,7 @@
 package week12_111296;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 public class File_Handling {
 	public static void main (String[] args) {
 	try {
@@ -14,6 +15,19 @@ public class File_Handling {
 		System.out.println("Error Occured");
 		e.printStackTrace();
 		
+	}
+	
+	try {
+		File obj2 = new File ("myfile.txt");
+		Scanner sc=new Scanner(obj2);
+		while (sc.hasNextLine()) {
+			String data=sc.nextLine();
+			System.out.println(data);
+		}
+		sc.close();
+	} catch (IOException e) {
+		System.out.println("Error");
+		e.printStackTrace();
 	}
 
 }
