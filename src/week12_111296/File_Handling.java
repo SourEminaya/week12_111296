f17e98ddccd5fcb70a6bf23ccae4cbd2d13bb303
NehemiaOroh <orohnehemia@gmail.com>
2025-11-26 11:00:10 +0700

    Write to myfile.txt

--- src/week12_111296/File_Handling.java
+++ src/week12_111296/File_Handling.java
@@ -2,6 +2,7 @@ package week12_111296;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.io.FileWriter;
 public class File_Handling {
 	public static void main (String[] args) {
 	try {
@@ -28,6 +29,17 @@ public class File_Handling {
 	} catch (IOException e) {
 		System.out.println("Error");
 		e.printStackTrace();
+	}
+	
+	try {
+		FileWriter wr = new FileWriter("myfile.txt");
+		wr.write("Nama saya John Doe\nSaya dari universitas multimedia nusantara\n");
+		wr.close();
+		System.out.println("Success");
+	} catch (IOException e) {
+		System.out.println("Error Occured");
+		e.printStackTrace();
+		
 	}
 
 }
